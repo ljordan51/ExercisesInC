a111b5761ac14446744d927627bb09d41c0a35c6
Lucky <ljordan51@gmail.com>
2020-02-14 14:30:59 -0500

    completed regex exercise

--- exercises/ex02.5/find_track.c
+++ exercises/ex02.5/find_track.c
@@ -7,6 +7,7 @@ Modified version of an example from Chapter 2.5 of Head First C.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -35,9 +36,30 @@ void find_track(char search_for[])
 // Finds all tracks that match the given pattern.
 //
 // Prints track number and title.
-void find_track_regex(char pattern[])
+int find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    int status;
+    regex_t re;
+
+    if (regcomp(&re, pattern, REG_NOSUB) != 0) {
+        puts("Error processing RegEx.");
+        return(0);      /* report error */
+    }
+    
+    for (int i=0; i<NUM_TRACKS; i++) {
+        status = regexec(&re, tracks[i], (size_t) 0, NULL, 0);
+        if (status == 0) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        } else if(status == REG_NOMATCH) {
+            puts("Error comparing RegEx to track names.");
+            regfree(&re);
+            return(0);
+        }
+        // need to handle error here
+    }
+    
+    regfree(&re);
+    return(1);
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -59,7 +81,7 @@ int main (int argc, char *argv[])
     rstrip(search_for);
 
     find_track(search_for);
-    //find_track_regex(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
