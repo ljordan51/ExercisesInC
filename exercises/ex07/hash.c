738b67f593d1affbb2ceedbf2887fe50c07afc0e
Lucky <ljordan51@gmail.com>
2020-03-31 21:52:10 -0400

    finished hw7

--- exercises/ex07/hash.c
+++ exercises/ex07/hash.c
@@ -178,8 +178,8 @@ int hash_hashable(Hashable *hashable)
 */
 int equal_int (void *ip, void *jp)
 {
-    // FILL THIS IN!
-    return 0;
+    return (*(int *)ip == *(int *)jp);
+    //return 0;
 }
 
 
@@ -192,8 +192,7 @@ int equal_int (void *ip, void *jp)
 */
 int equal_string (void *s1, void *s2)
 {
-    // FILL THIS IN!
-    return 0;
+    return ( strcmp( (char *) s1, (char *) s2 ) == 0);
 }
 
 
@@ -207,8 +206,7 @@ int equal_string (void *s1, void *s2)
 */
 int equal_hashable(Hashable *h1, Hashable *h2)
 {
-    // FILL THIS IN!
-    return 0;
+    h1->equal(h1->key, h2->key);
 }
 
 
@@ -296,7 +294,12 @@ Node *prepend(Hashable *key, Value *value, Node *rest)
 /* Looks up a key and returns the corresponding value, or NULL */
 Value *list_lookup(Node *list, Hashable *key)
 {
-    // FILL THIS IN!
+    while(list != NULL){
+      if(list->key == key){
+        return list->value;
+      }
+      list = list->next;
+    }
     return NULL;
 }
 
@@ -341,15 +344,21 @@ void print_map(Map *map)
 /* Adds a key-value pair to a map. */
 void map_add(Map *map, Hashable *key, Value *value)
 {
-    // FILL THIS IN!
+    Node* tmp;
+    if(map->lists[hash_hashable(key)%(map->n)]){
+      tmp = prepend(key, value, map->lists[hash_hashable(key)%(map->n)]);
+    }
+    else{
+      tmp = make_node(key, value, NULL);
+    }
+    map->lists[hash_hashable(key)%(map->n)] = tmp;
 }
 
 
 /* Looks up a key and returns the corresponding value, or NULL. */
 Value *map_lookup(Map *map, Hashable *key)
 {
-    // FILL THIS IN!
-    return NULL;
+    return list_lookup(map->lists[hash_hashable(key)%(map->n)], key);
 }
 
 
